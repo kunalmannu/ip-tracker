b8636ffe6434cfee470210ae6827e32ddb377007
kunalmannu <112188096+kunalmannu@users.noreply.github.com>
2023-04-12 10:19:36 -0400

    Add files via upload

---
+++ androinstall.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+clear
+echo "UPDATING"
+pkg update && apk upgrade -y
+clear
+echo "INSTALLING PHP"
+pkg install php
+clear
+echo "COMPLETED INSTALLATION
+
+   _______             _       _____       
+  |__   __|           | |     |_   _|      
+     | |_ __ __ _  ___| | __    | |  _ __  
+     | |  __/ _  |/ __| |/ /    | | |  _ \ 
+     | | | | (_| | (__|   <    _| |_| |_) |
+     |_|_|  \__,_|\___|_|\_\  |_____| .__/ 
+                                    | |    
+                                    |_|    
+
+Created By KunalMannu (v-2.0)"
+
+
+echo "Starting"
+bash ip-tracker.sh
+clear
